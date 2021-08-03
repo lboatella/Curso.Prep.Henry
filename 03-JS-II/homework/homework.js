b2f82bce486cbc81a11fe852a316d515c7aa0d3b
lboatella <lfb1961@hotmail.com>
2021-08-03 19:45:32 -0300

    03-js-ii luis parte 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -79,6 +79,8 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 10 || numero === 5;
+
 }
 
 function estaEnRango(numero) {
