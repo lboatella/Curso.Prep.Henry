2a44028146e5f1c32ab46cc923f24d5051c5f524
lboatella <lfb1961@hotmail.com>
2021-08-04 16:44:48 -0300

    04-JS-III/homework/homework.js parte1 Luis

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length-1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+
+  for (var i = 0; i < array.length; i++){
+    array[i]=array[i]+1;
+  }
+  return array;
 }
 
 
@@ -30,6 +38,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+   array.push(elemento);
+   return array;
 }
 
 
@@ -38,6 +48,9 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,6 +60,12 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  // var frase="";
+  // for (var i=0; i<palabras.length;i++){
+  //   frase=frase + palabras[i] + " ";
+  // }
+  // return frase;
+  return palabras.join (" ");
 }
 
 
@@ -54,6 +73,12 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (var i=0;i<array.length;i++){
+    if (array[i]=== elemento){
+      return true
+    }
+  }
+  return false
 }
 
 
